d86c3f5d5e6a38b62018d4aca6aa62db27057e5d
KJRG <kjrgalka@gmail.com>
2017-06-05 23:58:41 +0200

    Read data from separate training and test dataset and refactor code

--- mgr/src/main/java/io/github/kjrg/mgr/App.java
+++ mgr/src/main/java/io/github/kjrg/mgr/App.java
@@ -1,12 +1,7 @@
 package io.github.kjrg.mgr;
 
-import java.io.File;
 import java.io.IOException;
 
-import org.datavec.api.records.reader.RecordReader;
-import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
-import org.datavec.api.split.FileSplit;
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -18,48 +13,54 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.dataset.api.iterator.KFoldIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 /**
  * Main class of application.
- *
  */
 public class App {
 
-	private static final String DATASET_FILEPATH = "D:\\Praca magisterska\\dane\\cleveland_binary.csv";
-	private static final String COMMA_SEPARATOR = ";";
+	private static final String TRAINING_DATASET_FILEPATH = "D:\\Praca magisterska\\dane\\cleveland_binary_training_data.csv";
+	private static final String TEST_DATASET_FILEPATH = "D:\\Praca magisterska\\dane\\cleveland_binary_test_data.csv";
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println("Starting the application...");
 		
 		int numberOfInputs = 13;
-		int numberOfHiddenNeurons = 5;
+		int numberOfHiddenNeurons = 2;
 		int numberOfOutputs = 2;
 		
-		int numberOfSamples = 297;
-		int batchSize = numberOfSamples;
-		int iterations = 100;
-		int seed = 123;
-		int numberOfEpochs = 30;
-		double learningRate = 0.05;
-		
+		int trainingDatasetSize = 238;
+		int testDatasetSize = 59;
 		int labelColumnIndex = 13;
 		int numberOfLabels = 2;
 		
-		int numberOfFolds = 10;
+		int iterations = 100;
+		int seed = 123;
+		int numberOfEpochs = 100;
+		double learningRate = 0.05;
 
-		RecordReader recordReader = new CSVRecordReader(0, COMMA_SEPARATOR);
+		DataProvider dataProvider = new DataProvider();
+        DataSet trainDataset = null;
+    	DataSet testDataset = null;
+    	System.out.println("Reading data...");
 		try {
-			recordReader.initialize(new FileSplit(new File(DATASET_FILEPATH)));
+			trainDataset = dataProvider.readDatasetFromFile(TRAINING_DATASET_FILEPATH, trainingDatasetSize, labelColumnIndex, numberOfLabels);
+			testDataset = dataProvider.readDatasetFromFile(TEST_DATASET_FILEPATH, testDatasetSize, labelColumnIndex, numberOfLabels);
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		DataSetIterator datasetIterator = new RecordReaderDataSetIterator(recordReader, batchSize, labelColumnIndex, numberOfLabels);
-		DataSet dataset = datasetIterator.next();
+		
+		if (trainDataset == null) {
+			System.out.println("The training data could not be read.");
+			System.exit(1);
+		}
+		if (testDataset == null) {
+			System.out.println("The test data could not be read.");
+			System.exit(1);
+		}
 
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
 				.seed(seed)
@@ -88,26 +89,18 @@ public class App {
 		MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
         
-        KFoldIterator kFoldIterator = new KFoldIterator(numberOfFolds, dataset);
-        kFoldIterator.reset();
-        while (kFoldIterator.hasNext()) {
-        	DataSet trainDataset = kFoldIterator.next();
-        	DataSet testDataset = kFoldIterator.testFold();
+    	for (int n = 0; n < numberOfEpochs; n++) {
+    		model.fit(trainDataset);
+    	}
+    	
+    	Evaluation eval = new Evaluation(numberOfOutputs);
 
-        	for (int n = 0; n < numberOfEpochs; n++) {
-        		model.fit(trainDataset);
-        	}
-        	
-        	System.out.println("Fold " + kFoldIterator.cursor() + ", evaluate model...");
-        	Evaluation eval = new Evaluation(numberOfOutputs);
-
-        	INDArray features = testDataset.getFeatureMatrix();
-        	INDArray labels = testDataset.getLabels();
-        	INDArray predicted = model.output(features, false);
-        	
-        	eval.eval(labels, predicted);
-        	
-        	System.out.println(eval.stats());
-        }
+    	INDArray features = testDataset.getFeatureMatrix();
+    	INDArray labels = testDataset.getLabels();
+    	INDArray predicted = model.output(features, false);
+    	
+    	eval.eval(labels, predicted);
+    	
+    	System.out.println(eval.stats());
 	}
 }
---
+++ mgr/src/main/java/io/github/kjrg/mgr/DataProvider.java
@@ -0,0 +1,26 @@
+package io.github.kjrg.mgr;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+public class DataProvider {
+
+	private static final String COMMA_SEPARATOR = ";";
+
+	public DataSet readDatasetFromFile(String filepath, int batchSize, int labelColumnIndex, int numberOfLabels)
+			throws IOException, InterruptedException {
+		RecordReader recordReader = new CSVRecordReader(0, COMMA_SEPARATOR);
+		recordReader.initialize(new FileSplit(new File(filepath)));
+		DataSetIterator datasetIterator = new RecordReaderDataSetIterator(recordReader, batchSize, labelColumnIndex,
+				numberOfLabels);
+		DataSet dataset = datasetIterator.next();
+		return dataset;
+	}
+}
